f04c706f83c719ea9e91f24b48222631edf19dcd
CMarie <KickAss@Christas-MacBook-Pro.local>
2016-12-20 23:14:59 -0500

    gifs and rating

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,48 +1,83 @@
+
+// creating an inital array of topics
 var topics = ["beach", "dolphins", "seagulls", "sunsets", "surfing"]; 
 
+// function 1
 
-$("button").on("click", function() {
+//when anything with a class of gif is pressed, do this:
 
-var gif = $(this).attr("data-name"); 
-var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=dc6zaTOxFJmzC&limit=10";
+$(".gifBtn").on("click", function() {
 
-$.ajax({ 
-	url: queryURL, 
-	method: 'GET'
-}).done(function(response) {
-
-	console.log(response);
-	console.log(queryURL);
-
-	var
-
-	var imageUrl = response.data.url; 
-
-	var showGif = $("<img>").attr("src", imageUrl); 
+ 	//capture data-name for whatever is clicked 
+	var gif = $(this).attr("data-name"); 
 	
-	var gifDiv = $("<div>").attr("class", "gif-div"); 
-	
-	var p = $("<p>").text("Rating: " + response.data.rating);
-	
-		showGif.append("#gifs-view");
-	
-		p.append("#gifs-view");
-        
-      });
+	// queryURL
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif+ "&api_key=dc6zaTOxFJmzC";
+	// "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + gif;
 
+	$.ajax({ 
+		url: queryURL, 
+		method: 'GET'
+	}).done(function(response) {
 
-$("#buttons-view").on('click', function() {
-	event.preventDefault();
+		console.log(response);
+		// console.log(queryURL);
 
-	var newButton = $("#topic-input").val();
+		//shortcut to gif's json data
+		var results = response.data;
 
-	topics.push(newButton);
+			//for loop to run through the data
+			for (var i = 0; i < results.length; i++){
+				//designated div for gifs
+				var gifsHere = $("<div id = 'gifsHere'>");
+				//shortcut for gif's rating
+				var rating = results[i].rating; 
+				console.log(results[i].rating);
+				//designated line to list rating 
+				var gifInfo = $("<p>").text("Rating: " + rating);
 
+				var gifImage = $("<img>").attr("src", animated).attr("data-animate", animated);
+				
+				var animated = results[i].images.fixed_height.url;
+		
+		// var still = results[i].images.original_still;
+		// 	var animated = results[i].images.original.url;
+			
+			//put rating info in designated gif div 
+			gifsHere.prepend(gifInfo);
+			gifsHere.prepend(gifImage);
+
+			//puts new gif div in html gif div 
+			$("#gifs-view").prepend(gifsHere);
+		}; 
+
+	});
 });
 
 
-});
+// // function 2
+// // creating a new button from user's form input
+// function userGenButton(){
 
+// 	//empty div
+// 	$('#buttons-view').empty();
+
+// 	// creates a new button with the input as text and data-name
+// 	var newButton = $("<button>").addClass("gif").text("#topic-input").attr('data-name', "#topic-input");
+
+// 	//append new button to html buttons div
+// 	$("#buttons-view").append(newButton);
+
+// // function 3
+
+// $("#add-gif").on('click', function (event) {
+// 	event.preventDefault();
+
+// 	var newTopic = $("#topic-input").val().trim();
+
+// 	console.log(newTopic);
+
+// // });
 
 
 
